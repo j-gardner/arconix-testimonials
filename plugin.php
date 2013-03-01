5c07626e7a48b5aa02ae78957c6d0da9ac2470ad
John Gardner <john@arconixpc.com>
2013-02-28 23:05:35 -0500

    remove $defaults from included file
    for whatever reason it was causing an error when including the file that
    defined the variable.

--- plugin.php
+++ plugin.php
@@ -89,7 +89,39 @@ class Arconix_Testimonials {
      * @return array $defaults
      */
     function defaults() {
-        require_once( ACT_VIEWS_DIR . 'defaults.php' );
+        $prefix = '_act_'; // for use in the metabox id <--- WHICH UNFORTUNATELY IS NOT WORKING
+
+        $defaults = array(
+            'post_type' => array(
+                'slug' => 'testimonials',
+                'args' => array(
+                    'labels' => array(
+                        'name'                  => __( 'Testimonials',                              'act' ),
+                        'singular_name'         => __( 'Testimonial',                               'act' ),
+                        'add_new'               => __( 'Add New',                                   'act' ),
+                        'add_new_item'          => __( 'Add New Testimonial Item',                  'act' ),
+                        'edit'                  => __( 'Edit',                                      'act' ),
+                        'edit_item'             => __( 'Edit Testimonial Item',                     'act' ),
+                        'new_item'              => __( 'New Item',                                  'act' ),
+                        'view'                  => __( 'View Testimonial',                          'act' ),
+                        'view_item'             => __( 'View Testimonial Item',                     'act' ),
+                        'search_items'          => __( 'Search Testimonial',                        'act' ),
+                        'not_found'             => __( 'No testimonial items found',                'act' ),
+                        'not_found_in_trash'    => __( 'No testimonial items found in the trash',   'act' )
+                    ),
+                    'public'            => true,
+                    'query_var'         => true,
+                    'menu_position'     => 20,
+                    'menu_icon'         => ACT_IMAGES_URL . 'testimonials-16x16.png',
+                    'has_archive'       => false,
+                    'supports'          => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+                    'rewrite'           => array( 'slug' => 'testimonials', 'with_front' => false )
+                )
+            ),
+            'gravatar' => array(
+                'size' => 32 
+            )
+        );
         return apply_filters( 'arconix_testimonials_defaults', $defaults );
     }
 
